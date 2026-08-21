30893c7d9418a16cc53a8d87eea73db66dfde545
AnnaKotenkoInIT <anna.kotenko.kiev@gmail.com>
2026-08-21 23:16:13 +0300

    deleted servises for area and testimonials

--- src/controllers/area.controller.ts
+++ src/controllers/area.controller.ts
@@ -1,9 +1,13 @@
 import type { Request, Response } from 'express';
 
-import { getAllAreas, getAreaById } from '../services/area.service.ts';
+import prisma from '../../prisma/client.ts';
 
 export const getAllAreasController = async (_req: Request, res: Response) => {
-  const areas = await getAllAreas();
+  const areas = await prisma.area.findMany({
+    orderBy: {
+      name: 'asc',
+    },
+  });
 
   res.status(200).json({
     status: 200,
@@ -14,7 +18,11 @@ export const getAllAreasController = async (_req: Request, res: Response) => {
 export const getAreaByIdController = async (req: Request, res: Response) => {
   const id = req.params.id as string;
 
-  const area = await getAreaById(id);
+  const area = await prisma.area.findUnique({
+    where: {
+      id,
+    },
+  });
 
   if (!area) {
     res.status(404).json({
