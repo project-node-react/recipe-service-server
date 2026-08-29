05c1c8867f46c4d699058b04832432eb1f367ea8
Vitalii <mohara88s@gmail.com>
2026-08-29 13:13:39 +0300

    fix categories

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,9 +1,17 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from "express";
 import prisma from "../../prisma/client.ts";
 
-export const getCategories = async (req: Request, res: Response, next: NextFunction) => {
+export const getCategories = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
-    const categories = await prisma.category.findMany();
+    const categories = await prisma.category.findMany({
+      orderBy: {
+        name: "asc",
+      },
+    });
     res.status(200).json(categories);
   } catch (error) {
     next(error);
--- src/controllers/ingredients.controller.ts
+++ src/controllers/ingredients.controller.ts
@@ -1,9 +1,17 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from "express";
 import prisma from "../../prisma/client.ts";
 
-export const getIngredients = async (req: Request, res: Response, next: NextFunction) => {
+export const getIngredients = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
-    const ingredients = await prisma.ingredient.findMany();
+    const ingredients = await prisma.ingredient.findMany({
+      orderBy: {
+        name: "asc",
+      },
+    });
     res.status(200).json(ingredients);
   } catch (error) {
     next(error);
